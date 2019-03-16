b496de7519e3100823b1ae6e152b9063bba97c48
lwalther <lwalther@novareto.de>
2019-03-16 14:12:34 +0100

    Content-Types modifiziert

--- src/edi/plonevocab/content/vocabulary_folder.py
+++ src/edi/plonevocab/content/vocabulary_folder.py
@@ -1,63 +1,21 @@
 # -*- coding: utf-8 -*-
-# from plone.app.textfield import RichText
-# from plone.autoform import directives
 from plone.dexterity.content import Container
-# from plone.namedfile import field as namedfile
 from plone.supermodel import model
-# from plone.supermodel.directives import fieldset
-# from z3c.form.browser.radio import RadioFieldWidget
-# from zope import schema
+from zope import schema
 from zope.interface import implementer
+from zope.schema.vocabulary import SimpleVocabulary
 
-
-# from edi.plonevocab import _
-
+from edi.plonevocab import _
 
 class IVocabularyFolder(model.Schema):
     """ Marker interface and Dexterity Python Schema for VocabularyFolder
     """
-    # If you want, you can load a xml model created TTW here
-    # and customize it in Python:
 
-    # model.load('vocabulary_folder.xml')
-
-    # directives.widget(level=RadioFieldWidget)
-    # level = schema.Choice(
-    #     title=_(u'Sponsoring Level'),
-    #     vocabulary=LevelVocabulary,
-    #     required=True
-    # )
-
-    # text = RichText(
-    #     title=_(u'Text'),
-    #     required=False
-    # )
-
-    # url = schema.URI(
-    #     title=_(u'Link'),
-    #     required=False
-    # )
-
-    # fieldset('Images', fields=['logo', 'advertisement'])
-    # logo = namedfile.NamedBlobImage(
-    #     title=_(u'Logo'),
-    #     required=False,
-    # )
-
-    # advertisement = namedfile.NamedBlobImage(
-    #     title=_(u'Advertisement (Gold-sponsors and above)'),
-    #     required=False,
-    # )
-
-    # directives.read_permission(notes='cmf.ManagePortal')
-    # directives.write_permission(notes='cmf.ManagePortal')
-    # notes = RichText(
-    #     title=_(u'Secret Notes (only for site-admins)'),
-    #     required=False
-    # )
-
+    title = schema.TextLine(title=u"Titel")
 
 @implementer(IVocabularyFolder)
 class VocabularyFolder(Container):
     """
     """
+    getVocabulary(self):
+        return SimpleVocabulary([i.getTerm() for i in self.values()])
--- src/edi/plonevocab/content/vocabulary_term.py
+++ src/edi/plonevocab/content/vocabulary_term.py
@@ -1,63 +1,25 @@
 # -*- coding: utf-8 -*-
-# from plone.app.textfield import RichText
-# from plone.autoform import directives
+from plone.autoform import directives
 from plone.dexterity.content import Item
-# from plone.namedfile import field as namedfile
 from plone.supermodel import model
-# from plone.supermodel.directives import fieldset
-# from z3c.form.browser.radio import RadioFieldWidget
-# from zope import schema
+from zope import schema
 from zope.interface import implementer
+from zope.schema.vocabulary import SimpleTerm
 
-
-# from edi.plonevocab import _
-
+from edi.plonevocab import _
 
 class IVocabularyTerm(model.Schema):
     """ Marker interface and Dexterity Python Schema for VocabularyTerm
     """
-    # If you want, you can load a xml model created TTW here
-    # and customize it in Python:
-
-    # model.load('vocabulary_term.xml')
-
-    # directives.widget(level=RadioFieldWidget)
-    # level = schema.Choice(
-    #     title=_(u'Sponsoring Level'),
-    #     vocabulary=LevelVocabulary,
-    #     required=True
-    # )
-
-    # text = RichText(
-    #     title=_(u'Text'),
-    #     required=False
-    # )
-
-    # url = schema.URI(
-    #     title=_(u'Link'),
-    #     required=False
-    # )
-
-    # fieldset('Images', fields=['logo', 'advertisement'])
-    # logo = namedfile.NamedBlobImage(
-    #     title=_(u'Logo'),
-    #     required=False,
-    # )
-
-    # advertisement = namedfile.NamedBlobImage(
-    #     title=_(u'Advertisement (Gold-sponsors and above)'),
-    #     required=False,
-    # )
-
-    # directives.read_permission(notes='cmf.ManagePortal')
-    # directives.write_permission(notes='cmf.ManagePortal')
-    # notes = RichText(
-    #     title=_(u'Secret Notes (only for site-admins)'),
-    #     required=False
-    # )
-
+    
+    title = schema.TextLine(title=u"Titel")
+    value = schema.TextLine(title=u"Value", description=u"Keine Umlaute, Sonderzeichen, Leerzeichen, eindeutiger Wert innerhalb eines Ordners.")
+    token = schema.TextLine(title=u"Token", description=u"keine Umlaute, Sonderzeichen, Leerzeichen, oft werden für Value und Token die\
+                                                          selben Werte verwendet.")
 
 @implementer(IVocabularyTerm)
 class VocabularyTerm(Item):
     """
     """
+    getTerm(self):
+        return SimpleTerm(value=self.value, token=self.token, title=self.title)
