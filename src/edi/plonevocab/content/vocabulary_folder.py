7805ae3d0700a83e72f80244fbe64d0ee53c62a6
Lars Walther <lwalther@novareto.de>
2020-03-04 20:16:05 +0100

    Dynamic Vocabulary anstatt Static Vocabulary

--- src/edi/plonevocab/content/vocabulary_folder.py
+++ src/edi/plonevocab/content/vocabulary_folder.py
@@ -2,8 +2,9 @@
 from plone.dexterity.content import Container
 from plone.supermodel import model
 from zope import schema
-from zope.interface import implementer
+from zope.interface import implementer, provider
 from zope.schema.vocabulary import SimpleVocabulary
+from zope.schema.interfaces import IContextSourceBinder
 
 from edi.plonevocab import _
 
@@ -11,10 +12,12 @@ class IVocabularyFolder(model.Schema):
     """ Marker interface and Dexterity Python Schema for VocabularyFolder
     """
     title = schema.TextLine(title=u'Titel')
+    vocabid = schema.TextLine(title=u"Id des Vocabularies Beispiel: uvc.desinfektionsmittel.wirkung")
 
 @implementer(IVocabularyFolder)
 class VocabularyFolder(Container):
     """
     """
+    @provider(IContextSourceBinder)
     def getVocabulary(self):
         return SimpleVocabulary([i.getTerm() for i in self.values()])
